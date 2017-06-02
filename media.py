b7226a9ae3003f2756c322d2b69e4e0195c31c27
Safi Marroun <safi.marroun@gmail.com>
2017-06-02 14:17:02 +0300

    Update media.py

--- media.py
+++ media.py
@@ -1,10 +1,14 @@
 import webbrowser
 
-# Python Class created:
+# The Movie() class allow the creation of intances of a movie with four
+# inputs (title, storyline, image, and youtube) and opens youtube in
+# browswer:
 
 
 class Movie():
 
+        # This method initiates the class inputs; which are the title,
+        # storyline, poster, and youtube URL of the movie
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
         self.title = movie_title
@@ -12,5 +16,7 @@ class Movie():
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
+    # This method takes in the YouTube URL of a movie and opens it in the
+    # browswer
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
